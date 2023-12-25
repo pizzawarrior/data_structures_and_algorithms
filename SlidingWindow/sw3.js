72fb498be8a9752e8062069508ee34c425fdce88
pizzawarrior <i.norstad@gmail.com>
2023-12-25 17:12:12 +0900

    sliding window hashmap

--- SlidingWindow/sw3.js
+++ SlidingWindow/sw3.js
@@ -1,4 +1,4 @@
-// Given a string, find the length of the longest substring in it with no more than K DISTINCT characters.
+// Given a string, find the length of the longest substring in it with no more than (aka <= ) K DISTINCT characters.
 
 const longestSubstring = (k, str) => {
   let wStart = 0;
@@ -9,9 +9,11 @@ const longestSubstring = (k, str) => {
     const rightChar = str[wEnd];
     if (!(rightChar in charCount)) {
       charCount[rightChar] = 0;
-    }
+    } // else
     charCount[rightChar] += 1;
 
+    // the following line converts charCount's keys to an array
+    // while the array's length is > k we shrink the window until we have k distinct chars in our hashmap
     while (Object.keys(charCount).length > k) {
       const leftChar = str[wStart];
       charCount[leftChar] -= 1;
@@ -34,9 +36,9 @@ console.log(longestSubstring(3, "cbbebi"));
 
 // Pseudocode:
 /*
-1.) First, we will insert characters from the beginning of the string until we have ‘K’ distinct characters in the HashMap.
+1.) First, we will insert characters into the hash map from the beginning of the string until we have ‘K’ distinct characters in the HashMap.
 2.) These characters will constitute our sliding window. We are asked to find the longest such window having no more than ‘K’ distinct characters. We will remember the length of this window as the longest window so far.
-3.) Then we will keep adding one character in the sliding window (i.e. slide the window ahead), in a stepwise fashion.
+3.) Then we will keep adding one character in the sliding window (i.e. slide the window to the right), in a stepwise fashion.
 4.) In each step, we will try to shrink the window from the beginning if the count of distinct characters in the HashMap is larger than ‘K’. We will shrink the window until we have no more than ‘K’ distinct characters in the HashMap. This is needed as we intend to find the longest window.
 5.) While shrinking, we’ll decrement the frequency of the character going out of the window and remove it from the HashMap if its frequency becomes zero.
 6.) At the end of each step, we’ll check if the current window length is the longest so far, and if so, remember its length.
--- SlidingWindow/sw4.js
+++ SlidingWindow/sw4.js
@@ -1,11 +1,48 @@
-let map1 = new Map([
-  [1, 10],
-  [2, 20],
-  [3, 30],
-  [4, 40],
-]);
-console.log("Map1: ");
-console.log(map1);
-console.log(map1.get(2)); // 20
-console.log(map1.has(4)); // true
-console.log(map1.has(5)); // false
+/*
+Given an array of characters where each character represents a fruit tree, you are given two baskets and your goal is to put the maximum number of fruits in each basket. The only restriction is that each basket can have only one type of fruit.
+You can start with any tree, but once you have started you can’t skip a tree. You will pick one fruit from each tree until you cannot, i.e., you will stop when you have to pick from a third fruit type.
+Write a function to return the maximum number of fruits in both the baskets.
+(This is virtually the same as the previous problem-- find the length of the longest substring with no more than 2 unique values)
+*/
+
+const fruitBaskets = (arr) => {
+  let start = 0;
+  let maxLen = 0;
+  let fruitCount = {};
+
+  for (let end = 0; end <= arr.length; end++) {
+    const rightFruit = arr[end];
+    if (!(rightFruit in fruitCount)) {
+      fruitCount[rightFruit] = 0;
+    }
+    fruitCount[rightFruit] += 1;
+
+    while (Object.keys(fruitCount).length > 2) {
+      const leftFruit = arr[start];
+      fruitCount[leftFruit] -= 1;
+      if (fruitCount[leftFruit] === 0) {
+        delete fruitCount[leftFruit];
+      }
+      start += 1;
+    }
+    maxLen = Math.max(maxLen, end - start + 1);
+  }
+  return maxLen;
+};
+console.log(fruitBaskets(["A", "B", "C", "A", "A", "C"]));
+// Output: 3 -> ['C', 'A', 'C']
+
+// logic:
+// iterate thru arr
+// add right 'fruit' to hashmap with value of 0
+// if fruit in hashmap
+// increment fruitCount[rightFruit] by +1
+// while fruitCount.keys.length > 2:
+// designate leftFruit as start
+// decriment fruitCount[leftFruit] -1
+// if fruitCount[leftChar] is equal to 0
+// delete fruitCount[leftChar] from fruitCount
+// increment start +1
+// declare maxLen = Math.max(maxLen, end - start +1)
+
+// return maxLen
