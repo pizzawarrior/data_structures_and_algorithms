1f4ab9576544250be84d9ed5e60bfcb8a4f30666
pizzawarrior <i.norstad@gmail.com>
2023-12-26 16:55:27 +0900

    sliding window hashmap, longest substr, abstract

--- SlidingWindow/sw6.js
+++ SlidingWindow/sw6.js
@@ -6,3 +6,6 @@ const longestSub = (str, k) => {};
 console.log(longestSub("abccde", 1));
 //Output: 3
 // Explanation: Replace the 'b' or 'd' with 'c' to have the longest repeating substring "ccc".
+
+// Abstract: track start, end, and maxLen of str. iterate thru, find longest substring of repeating letters using hashmap. will need to do some magic to keep track of indexes and adding and subtracting k to current index. return maxLen + k
+//Example: start with counting char at index 0. if char at index 0 +1 === previous char, OR char at curr index +1 +k === curr char, then maxLen = Math.max(maxLen,...). otherwise increment start by +1, decrement rightChar by -1, and repeat.
