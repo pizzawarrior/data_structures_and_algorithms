dc324ab678727ed9045f80581795e60a45ed69c6
pizzawarrior <i.norstad@gmail.com>
2023-12-26 18:26:20 +0900

    solution

--- SlidingWindow/sw6.js
+++ SlidingWindow/sw6.js
@@ -15,11 +15,20 @@ const longestSub = (str, k) => {
     }
     charCount[rightChar] += 1;
     maxRepeat = Math.max(maxRepeat, charCount[rightChar]);
+
+    if (end - start + 1 - maxRepeat > k) {
+      leftChar = str[start];
+      charCount[leftChar] -= 1;
+      start += 1;
+    }
+    maxLen = Math.max(maxLen, end - start + 1);
   }
+  return maxLen;
 };
 console.log(longestSub("abccde", 1));
 //Output: 3
 // Explanation: Replace the 'b' or 'd' with 'c' to have the longest repeating substring "ccc".
+// Time Complexity: O(n)- Iterate thru array 1 time; n = num of chars in string
+// Space Complexity: O(1)- Technically it would be O(26) worst case, having to store values for each letter of alphabet in hashmap, which resolves to O(1).
 
-// Abstract: track start, end, and maxLen of str. iterate thru, find longest substring of repeating letters using hashmap. will need to do some magic to keep track of indexes and adding and subtracting k to current index. return maxLen + k
-//Example: start with counting char at index 0. if char at index 0 +1 === previous char, OR char at curr index +1 +k === curr char, then maxLen = Math.max(maxLen,...). otherwise increment start by +1, decrement rightChar by -1, and repeat.
+// Abstract: track start, end, and maxLen of str. iterate thru str, find longest substring of repeating letters using hashmap. curr window size is from start to end-- we find a letter that repeats maxRepeat times and the remaining letters we should replace. if remaining letters are > k, shrink window from start. return maxLen.
