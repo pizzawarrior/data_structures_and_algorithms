78ffb2d8dca815f40187b37386bc047eb9f3a5d9
pizzawarrior <i.norstad@gmail.com>
2024-01-09 16:52:42 -0800

    too close for javascript, switching over to python

---
+++ twoPointers/tp1.py
@@ -0,0 +1,9 @@
+# Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target.
+# Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
+
+
+def pair_of_indexes(arr, target):
+    pass
+
+
+print(pair_of_indexes([1, 2, 3, 4, 6]), 6)  # Output: [1, 3]
