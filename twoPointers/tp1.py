0f784b14c3a80a52e79f3577c9409ca3f5aa0756
pizzawarrior <i.norstad@gmail.com>
2024-01-09 19:30:17 -0800

    2 pointer solution as binary search and as hashtable

--- twoPointers/tp1.py
+++ twoPointers/tp1.py
@@ -2,23 +2,69 @@
 # Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
 
 
-def pair_of_indexes(arr, target):
-    left = 0
-    right = len(arr) - 1
+# Binary Search method:
+# def pair_of_indexes(arr, target):
+#     left = 0
+#     right = len(arr) - 1
 
-    while left < right:
-        current_sum = arr[left] + arr[right]
+#     while left < right:
+#         current_sum = arr[left] + arr[right]
 
-        if current_sum == target:
-            return [left, right]
+#         if current_sum == target:
+#             return [left, right]
 
-        if current_sum > target:
-            right -= 1
+#         if current_sum > target:
+#             right -= 1
 
+#         else:
+#             left += 1
+
+#     return [-1, -1]
+
+
+# print(pair_of_indexes([1, 2, 3, 4, 6], 6))  # Output: [1, 3]
+# Time Complexity: O(n) where n = the number of elements in the input array
+# Space Complexity: O(1) constant space
+
+"""
+Abstract:
+-designate a start point and an end pointer as indexes of the start and end of input list
+-use while loop to create boundaries, with base case of when start becomes equal to end
+    -create a current_sum variable that stores current value of the sum of start and end
+    -if current_sum == target return start, end
+    -if current_sum > target decrement end by -1
+    -else increment start by +1
+-else return [-1, -1]
+"""
+
+# Alternative: Use a HashTable
+
+
+def alt_pair_of_indexes(arr, target):
+    nums = {}
+    for i, num in enumerate(arr):
+        if target - num in nums:
+            return [nums[target - num], i]
         else:
-            left += 1
-
+            nums[arr[i]] = i
     return [-1, -1]
 
 
-print(pair_of_indexes([1, 2, 3, 4, 6], 6))  # Output: [1, 3]
+def main():
+    print(alt_pair_of_indexes([1, 2, 3, 4, 6], 6))
+    print(alt_pair_of_indexes([2, 5, 9, 11], 47))
+
+
+main()
+# Time Complexity: O(n) where n = the number of elements in the input array
+# Space Complexity: O(n) where worst case we will append n numbers to the hashtable
+
+"""
+Abstract:
+-create a result dict to store current index and value of value in array
+-iterate thru the enumeration of array using i and num as iterators
+    -if 'y' ('target - x') is in hashtable:
+        -we have found pair: return y, i
+    -otherwise if 'y' is not in hashtable then insert 'x' into hashtable with value as i
+-if none of the above return [-1, -1]
+"""
