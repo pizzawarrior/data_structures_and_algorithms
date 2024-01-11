65971a5c743669dbc43bfea21cdea6a775690608
pizzawarrior <i.norstad@gmail.com>
2024-01-10 19:28:48 -0800

    two pointer #3

---
+++ twoPointers/tp3.py
@@ -0,0 +1,10 @@
+"""
+Given an unsorted array of numbers and a target ‘key’, remove all instances of ‘key’ in-place and return the new length of the array.
+"""
+
+
+def clean_length(lst, key):
+    pass
+
+
+print(clean_length([3, 2, 3, 6, 3, 10, 9, 3], 3))  # Output: 4
