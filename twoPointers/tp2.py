3a45d62d9fd8e3f3d08c61a9a9f93c11784adb44
pizzawarrior <i.norstad@gmail.com>
2024-01-10 17:27:33 -0800

    two pointer #2, solution

--- twoPointers/tp2.py
+++ twoPointers/tp2.py
@@ -10,10 +10,33 @@ Explanation: The first four elements after removing the duplicates will be [2, 3
 
 
 def dedupe(lst):
-    pass
+    if not lst:
+        return 0
+
+    i, j = 0, 1
+
+    while j < len(lst):
+        if lst[j] != lst[i]:
+            i += 1
+            lst[i] = lst[j]
+        j += 1
+
+    return i + 1
 
 
 print(dedupe([2, 3, 3, 3, 6, 9, 9]))  # Output: 4
+"""
+Abstract:
+If list is falsey return 0
+Set 2 pointers, i and j equal to 0, 1
+While j is < the length of lst:
+    if list at j doesn't equal list at i:
+        move left pointer (i) up one
+        REPLACE lst at i with lst at j: this effectively creates a new sorted list to the left of i
+    move right pointer (j) up one
+    (Note: j iterates all the way thru the list, while i only moves up when there is not a match. At the end of the above input the function exits with lst arranged like so: [2, 3, 6, 9, 6, 9, 9]), with i having only made it to index 3. This is why we return i + 1)
+Return i + 1
+"""
 
 
 # Alternative: let's have fun with recursion: THIS IS NOT A TWO-POINTER!
