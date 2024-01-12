e24c1a64d5228e23fe0868d886c3404227c561cb
pizzawarrior <i.norstad@gmail.com>
2024-01-12 12:01:54 -0800

    lets do some fast pointers with a linked list party

---
+++ 4.FastSlowPointers/fsp1.js
@@ -0,0 +1,10 @@
+// Given the head of a Singly LinkedList, write a function to determine if the LinkedList has a cycle in it or not.
+
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+const hasCycle = function (head) {};
