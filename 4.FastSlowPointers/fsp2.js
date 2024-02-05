df371926061395d6d3ea7a56f9351cbf61c593ff
pizzawarrior <i.norstad@gmail.com>
2024-02-05 09:27:39 -0800

    fast slow pointer LL calc cycle start position complete

--- 4.FastSlowPointers/fsp2.js
+++ 4.FastSlowPointers/fsp2.js
@@ -10,33 +10,52 @@ class Node {
   }
 }
 
-const find_cycle_start = (head) => {
-  let pointer1 = head,
-    pointer2 = head;
+const findCycleStart = (head) => {
+  let cycleLength;
+  let fast = head,
+    slow = head;
 
-  while (pointer1 !== null && pointer1.next !== null) {
-    pointer1 = pointer1.next.next;
-    pointer2 = pointer2.next;
+  while (fast !== null && fast.next !== null) {
+    fast = fast.next.next;
+    slow = slow.next;
 
-    if (pointer2 === pointer1) {
-      return calcCycleLength(pointer2);
+    if (slow === fast) {
+      cycleLength = calcCycleLength(slow);
+      break;
     }
   }
-  return 0;
+  return cycleStart(head, cycleLength);
 };
 
-function calcCycleLength(pointer2) {
-  let current = pointer2,
+function calcCycleLength(slow) {
+  let current = slow,
     cycleLength = 0;
 
   while (true) {
     current = current.next;
     cycleLength += 1;
-    if (current === pointer2) {
+    if (current === slow) {
       break;
     }
   }
   return cycleLength;
+}
+
+function cycleStart(head, cycleLength) {
+  let pointer1 = head,
+    pointer2 = head;
+
+  // now that we know the cycle length, increment pointer2 by that value
+  while (cycleLength > 0) {
+    pointer2 = pointer2.next;
+    cycleLength -= 1;
+  }
+  // increment both pointers until they meet. where they meet is the start position of the cycle.
+  while (pointer1 !== pointer2) {
+    pointer1 = pointer1.next;
+    pointer2 = pointer2.next;
+  }
+  return pointer1;
 }
 
 const head = new Node(1);
@@ -47,13 +66,19 @@ head.next.next.next.next = new Node(5);
 head.next.next.next.next.next = new Node(6);
 
 head.next.next.next.next.next.next = head.next.next;
-console.log(`Cycle starts: ${find_cycle_start(head).value}`);
+console.log(`Cycle starts: ${findCycleStart(head).value}`);
 
 head.next.next.next.next.next.next = head.next.next.next;
-console.log(`Cycle starts: ${find_cycle_start(head).value}`);
+console.log(`Cycle starts: ${findCycleStart(head).value}`);
 
 head.next.next.next.next.next.next = head;
-console.log(`Cycle starts: ${find_cycle_start(head).value}`);
+console.log(`Cycle starts: ${findCycleStart(head).value}`);
+/*
+Output:
+Cycle starts: 3
+Cycle starts: 4
+Cycle starts: 1
+*/
 
 /*
 Abstract: If we know the length of the LL cycle we can find the start position (value) of the cycle by:
