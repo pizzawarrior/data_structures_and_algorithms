b187dc0ecc417c952de92af0cc502ed474286f09
pizzawarrior <i.norstad@gmail.com>
2024-02-05 08:53:27 -0800

    fast slow pointer linked list find start of cycle

--- 4.FastSlowPointers/fsp2.js
+++ 4.FastSlowPointers/fsp2.js
@@ -30,3 +30,14 @@ console.log(`Cycle starts: ${find_cycle_start(head).value}`);
 
 head.next.next.next.next.next.next = head;
 console.log(`Cycle starts: ${find_cycle_start(head).value}`);
+
+/*
+Abstract: If we know the length of the LL cycle we can find the start position (value) of the cycle by:
+- using 2 pointers, pointer1 and pointer2
+- initialize the pointers to start at the head of LL
+- find the length of the cycle as demo'd in the last challenge (fs1V2.js)
+- let's say the length of the cycle is 'k'. move pointer2 k steps
+- increment pointer1 and pointer2 until their values are equal
+- because pointer2 is k nodes ahead of pointer1, it means that when they meet up pointer 2 must have completed 1 cycle.
+- where they meet is he start of the cycle.
+*/
