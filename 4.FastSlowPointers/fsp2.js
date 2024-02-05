8feb32e080825e2a002302ecb61f81a72de1f7f5
pizzawarrior <i.norstad@gmail.com>
2024-02-04 17:18:58 -0800

    new day, new linked list

---
+++ 4.FastSlowPointers/fsp2.js
@@ -0,0 +1,29 @@
+/*
+Problem Statement:
+Given the head of a Singly LinkedList that contains a cycle, write a function to find the starting node of the cycle.
+*/
+
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+const find_cycle_start = (head) => {};
+
+head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+head.next.next.next.next.next = new Node(6);
+
+head.next.next.next.next.next.next = head.next.next;
+console.log(`Cycle starts: ${find_cycle_start(head).value}`);
+
+head.next.next.next.next.next.next = head.next.next.next;
+console.log(`Cycle starts: ${find_cycle_start(head).value}`);
+
+head.next.next.next.next.next.next = head;
+console.log(`Cycle starts: ${find_cycle_start(head).value}`);
