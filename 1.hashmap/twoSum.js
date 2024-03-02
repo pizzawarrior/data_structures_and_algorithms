cdc6a13fce9bfc02c236ce494dde2a63326e5350
pizzawarrior <i.norstad@gmail.com>
2024-03-02 14:13:16 -0800

    hashmap solution

--- 1.hashmap/twoSum.js
+++ 1.hashmap/twoSum.js
@@ -1,19 +1,30 @@
 /*
-beloved two-sum: Given an array of integers nums and an integer target,
+Beloved two-sum: Given an array of integers nums and an integer target,
 return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution,
 and you may not use the same element twice.
 */
 
-const hashmap = (nums, target) => {
+const twoSum = (nums, target) => {
   let mapObj = {};
 
   for (let i = 0; i < nums.length; i++) {
     let compliment = target - nums[i];
     if (mapObj.hasOwnProperty(compliment)) {
-      return [];
+      return [mapObj[compliment], i];
     }
+    mapObj[nums[i]] = i;
   }
 };
 
 console.log(twoSum([41, 3, 7, 1, 19], 4));
+//Output: [1, 3]
+
+/*
+Abstract:
+-create hashmap object to store compliment values and their index
+-iterate through array
+-define compliment as target - current num
+-if hashmap contains compliment, return the value of the compliment from the hashmap
+-otherwise add current value to hashmap with value as index
+*/
