cab263e67332822bae436de86e561799e9723248
pizzawarrior <i.norstad@gmail.com>
2024-03-10 16:19:14 -0700

    problem with solution

--- 6.CyclicSort/cs3.py
+++ 6.CyclicSort/cs3.py
@@ -14,17 +14,35 @@ Input: [2, 4, 1, 2]
 Output: 3
 """
 
+# iterations:
+# [2, 3, 2, 1]
+# [2, 3, 2, 1]
+
 
 def find_missing_nums(nums):
-    pass
+    i = 0
+
+    while i < len(nums):
+        j = nums[i] - 1
+        if nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i += 1
+    print(f"nums: {nums}")
+
+    missing_nums = []
+    for i in range(len(nums)):
+        if nums[i] != i + 1:
+            missing_nums.append(i + 1)
+    return missing_nums
 
 
-print(find_missing_nums([2, 3, 2, 1]))  # [1, 2, 2, 3]
-# Output: 4
+print(find_missing_nums([2, 3, 1, 8, 2, 3, 5, 1]))  # sorted = [1, 2, 3, 1, 5, 3, 2, 8]
+# Output: [4, 6, 7]
 
 """
 Abstract:
 -we want to sort the array first
 -then iterate through sorted array
--wherever the num in the sorted array does not equal i -1 we want to return that number
+-wherever num in the sorted array does not equal i -1 we want to return num
 """
