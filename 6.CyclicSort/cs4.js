67d4076cc54380b626ab657da1ab9e2189e8b1ad
pizzawarrior <i.norstad@gmail.com>
2024-03-10 17:15:06 -0700

    function statement

--- 6.CyclicSort/cs4.js
+++ 6.CyclicSort/cs4.js
@@ -12,3 +12,13 @@ Example 2:
 Input: [2, 1, 3, 3, 5, 4]
 Output: 3
 */
+
+function findRepeat(arr) {}
+
+console.log(findRepeat([1, 4, 4, 3, 2]));
+
+/*Abstract:
+-sort array
+-loop thru
+-as soon as we find a num that is not in te right spot we return it
+*/
